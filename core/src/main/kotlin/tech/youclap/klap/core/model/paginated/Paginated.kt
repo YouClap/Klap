6cbae3a98bd0c1dfae68dcd0485ec4b74e429464
Renato Almeida <renato.m.alm@gmail.com>
2019-12-03 12:24:20 +0000

    feat: Helper method for Paginated

--- core/src/main/kotlin/tech/youclap/klap/core/model/paginated/Paginated.kt
+++ core/src/main/kotlin/tech/youclap/klap/core/model/paginated/Paginated.kt
@@ -3,4 +3,29 @@ package tech.youclap.klap.core.model.paginated
 data class Paginated<T>(
     val data: T,
     val page: Page
-)
+) {
+    companion object {
+        fun <D> from(result: D, page: Int, per: Int, total: Long): Paginated<D> {
+
+            val position = Position(
+                current = page,
+                max = (total / per).toInt()
+            )
+
+            val data = Data(
+                per = per,
+                total = total
+            )
+
+            val paginationPage = Page(
+                position = position,
+                data = data
+            )
+
+            return Paginated(
+                data = result,
+                page = paginationPage
+            )
+        }
+    }
+}
